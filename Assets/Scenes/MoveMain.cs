8e5102ee71642f5565d186f97b45240e436be368
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-07-23 22:51:35 +0900

    save and load name

--- Assets/Scenes/MoveMain.cs
+++ Assets/Scenes/MoveMain.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MoveMain : MonoBehaviour
 {
+    public InputField inputField;
+
     public void OnClickSubmitButton()
     {
+        // 入力がないのにボタンを押された時
+        if(inputField.text == null || inputField.text == ""){
+            Debug.Log("Name Null");
+            return;
+        }
+        Debug.Log (inputField.text);
+        // 入力の保存
+        PlayerPrefs.SetString("Name", inputField.text);
+        // 遷移
         SceneManager.LoadScene("SampleScene");
     }
 

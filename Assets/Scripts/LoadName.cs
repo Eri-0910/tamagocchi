79008cdd1a347de76a574789363fc4636998ad35
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-12-08 17:18:40 +0900

    refactor loadname

--- Assets/Scripts/LoadName.cs
+++ Assets/Scripts/LoadName.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using static SceneNames;
 
 public class LoadName : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        name = PlayerPrefs.GetString("Name", "ナナシのたまごっち");
-        GetComponent<Text>().text = name;
+        // 名前の取得
+        string name = PlayerPrefs.GetString("Name");
+
+        // キャラクターが存在するかどうか(=初回起動かどうか)で、どう遷移するか変える
+        if (name == null)
+        {
+            // 遷移
+            SceneManager.LoadScene(NAME_SCENE);
+        } else
+        {
+            // 設定
+            GetComponent<Text>().text = name;
+        }
     }
 
 }

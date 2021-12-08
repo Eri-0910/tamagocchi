78211aacf89c90232f713d6ba4098e42f3e70abf
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-12-08 16:23:29 +0900

    add comment to utils

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -4,11 +4,18 @@ using System.Globalization;
 public class Utils
 {
     private static readonly string dateFormat = "M/d/yyyy h:m:s tt";
+
+    /// <summary>
+    /// 文字列をDateTimeに変換
+    /// </summary>
     public static DateTime StringToDateTime(string dateString)
     {
         return DateTime.ParseExact(dateString, dateFormat, new CultureInfo("en-US")).Date;
     }
 
+    /// <summary>
+    /// DateTimeを文字列に変換
+    /// </summary>
     public static string DateTimeToString(DateTime date)
     {
         return date.ToString(dateFormat);

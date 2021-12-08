32856b3e10adf036ce02f8e2485c8eb25ff16cf0
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-12-08 16:21:35 +0900

    add DateTimeToString

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -3,8 +3,14 @@ using System.Globalization;
 
 public class Utils
 {
+    private static readonly string dateFormat = "M/d/yyyy h:m:s tt";
     public static DateTime StringToDateTime(string dateString)
     {
-        return DateTime.ParseExact(dateString, "M/d/yyyy h:m:s tt", new CultureInfo("en-US")).Date;
+        return DateTime.ParseExact(dateString, dateFormat, new CultureInfo("en-US")).Date;
+    }
+
+    public static string DateTimeToString(DateTime date)
+    {
+        return date.ToString(dateFormat);
     }
 }
